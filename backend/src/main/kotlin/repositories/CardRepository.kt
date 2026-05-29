13e6dcf20da1082846beb10b49187ac11cc19229
Thomas <t.berghuis@st.hanze.nl>
2026-05-30 00:37:17 +0200

    refactor: Update mocked card repository

--- backend/src/main/kotlin/repositories/CardRepository.kt
+++ backend/src/main/kotlin/repositories/CardRepository.kt
@@ -9,10 +9,49 @@ class CardRepository {
 
     fun findAll(): List<Card> {
         return listOf(
-            Card(1, "Pokemon", "tcgdex",
-                "Charizard", "151", "3",
-                Rarity.RARE, "filler", 100.00, CurrencyCode.EUR,
-                "json", LocalDateTime.now(), LocalDateTime.now())
+            Card(
+                id = 1,
+                gameType = "Pokemon",
+                externalApiId = "tcgdex",
+                name = "Charizard",
+                setCode = "151",
+                collectorNumber = "3",
+                rarity = Rarity.RARE,
+                imageUrl = "filler",
+                price = 100.00,
+                currency = CurrencyCode.EUR,
+                rawJson = "json",
+                createdAt = LocalDateTime.now(),
+                updatedAt = LocalDateTime.now()
+            ), Card(
+                id = 1,
+                gameType = "Pokemon",
+                externalApiId = "tcgdex",
+                name = "Blastoise",
+                setCode = "151",
+                collectorNumber = "2",
+                rarity = Rarity.RARE,
+                imageUrl = "filler",
+                price = 90.00,
+                currency = CurrencyCode.EUR,
+                rawJson = "json",
+                createdAt = LocalDateTime.now(),
+                updatedAt = LocalDateTime.now()
+            ), Card(
+                id = 1,
+                gameType = "Pokemon",
+                externalApiId = "tcgdex",
+                name = "Venusaur",
+                setCode = "151",
+                collectorNumber = "5",
+                rarity = Rarity.RARE,
+                imageUrl = "filler",
+                price = 100.00,
+                currency = CurrencyCode.EUR,
+                rawJson = "json",
+                createdAt = LocalDateTime.now(),
+                updatedAt = LocalDateTime.now()
+            )
         )
     }
 }

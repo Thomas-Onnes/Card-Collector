d0a94788fd22084535b6c3e17aa07c2865e6b752
Thomas <t.berghuis@st.hanze.nl>
2026-05-30 00:38:23 +0200

    Chore: Update main.kt test code

--- backend/src/main/kotlin/Main.kt
+++ backend/src/main/kotlin/Main.kt
@@ -1,9 +1,17 @@
-import controllers.CardController
-import repositories.CardRepository
-import services.CardService
+import config.Environment
 import core.App
+import database.DatabaseConnection
+import database.MigrationRunner
 
 fun main() {
+    val connection = DatabaseConnection.getConnection()
+    println("Database connected")
+
+    val migrationRunner = MigrationRunner()
+    migrationRunner.run()
+
     println("Card Collector Backend Running")
     App().start()
+
+    connection.close()
 }
